13403ac5a464e6b6d2af7c2bc60cacce99b8496f
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-14 16:12:18 -0500

    finished classroom work for examples:

--- public/js/switch.js
+++ public/js/switch.js
@@ -11,6 +11,24 @@ switch (color) {
     // TODO: create a case statement that will handle every color except indigo and violet
     // TODO: when a color is encountered log a message that tells the color, and an object of that color
     //       example: Blue is the color of the sky.
+    case 'red':
+      console.log('Blood is red');
+      break;
+    case 'orange':
+      console.log('Oranges are orange');
+      break;
+    case 'yellow':
+      console.log('Corn is yellow');
+      break;
+    case 'green':
+      console.log('Money is green!');
+      break;
+    case 'blue':
+      console.log('The sky is blue');
+      break;
+    default:
+      console.log('I don\'t know anything by that color');
+      break;
 
     // TODO: create a default case that will catch indigo and violet
     // TODO: for the default case, log: I do not know anything by that color.
