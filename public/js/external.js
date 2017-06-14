87e0cfed925df9531c8b7b14fae93b4b4a5951d0
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-14 11:34:02 -0500

    added an alert

--- public/js/external.js
+++ public/js/external.js
@@ -1,5 +1,7 @@
 console.log("Hello from external JS")
 
+alert("Welcome to Codeup!")
+
 var userInput = prompt("What is your favorite color?");
 
 alert("No way! " + userInput.toUpperCase() + " is my favorite color too! Did we just become best friends?")
