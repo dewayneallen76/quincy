0c35e801ec243e53de4f782ae8948d057c405073
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-14 11:32:03 -0500

    added prompt for user input and alert with message containing user input

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,5 @@
 console.log("Hello from external JS")
+
+var userInput = prompt("What is your favorite color?");
+
+alert("No way! " + userInput.toUpperCase() + " is my favorite color too! Did we just become best friends?")
