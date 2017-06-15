1e715992ce84af06277093fc352df1e42223a5cb
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-15 15:50:32 -0500

    working on break/continue loop

--- public/js/break_and_continue.js
+++ public/js/break_and_continue.js
@@ -1,8 +1,20 @@
 "use strict";
 
-var userInput = prompt('Enter a number between 1 and 50');
 
-if(userInput % 2 != 0) {
-  prompt('Enter an odd number please');
-  break;
-}
+var userInput = prompt('Enter an odd number between 1 and 50');
+console.log(userInput);
+
+  for (var i = 1; i < 50; i++) {
+	   if (i % 2 === 0) {
+        continue;
+	   }
+    if (userInput === i) {
+    	console.log('We have reached your number and will skip number: ' + i);
+    } else {
+    	console.log('Here is an odd number: ' + i);
+    	continue;
+    }
+   	if (i >= 49) {
+    	break;
+    }
+  }
