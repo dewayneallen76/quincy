b19b0a1597aa07168fb774969de279239c9c737e
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-19 10:00:53 -0500

    added array of names, logged each name individually

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -2,8 +2,14 @@
     "use strict";
     console.log("JS connected");
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    var names = ['Dewayne', 'Kristy', 'Lola', 'MaryAlice'];
 
     // TODO: Create a log statement that will log the number of elements in the names array.
-
+    console.log(names.length);
     // TODO: Create log statements that will print each of the names array elements individually.
+    console.log('There are ' + names.length + ' names in the names array');
+    console.log('The first name is: ' + names[0]);
+    console.log('The second name is: ' + names[1]);
+    console.log('The third name is: ' + names[2]);
+    console.log('The fourth name is: ' + names[3]);
 })();
