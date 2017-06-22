55270790e567fbe932c18085d090fc8bcb3c64b9
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-22 17:02:15 -0500

    added files to start a new calculator

--- public/js/calculator2.js
+++ public/js/calculator2.js
@@ -1,4 +1,34 @@
 (function() {
   'use strict';
-  
+  console.log('linked');
+
+  var leftInput = document.getElementById('leftInput');
+  var operatorInput = document.getElementById('operatorInput');
+  var rightInput = document.getElementById('rightInput');
+  var number = document.getElementsByClassName('number');
+  var operator = document.getElementsByClassName('operator');
+
+  for(var i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', buttonHandler)
+  }
+
+  for(var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', buttonHandler);
+  }
+
+  function buttonHandler() {
+    if (this.className.includes('number')) {
+      var displayValue = leftInput.getAttribute('value');
+      displayValue += this.innerText;
+      leftInput.setAttribute('value', displayValue);
+    } else if (this.className.includes('operator')) {
+      operatorInput.setAttribute('value', this.innerText);
+    } else if (operatorInput.value != '') {
+      var displayValue = rightInput.getAttribute('value');
+      displayValue += this.innerText;
+      rightInput.setAttribute('value', displayValue);
+    }
+  };
+
+
 }());
