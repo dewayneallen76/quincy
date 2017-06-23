709324ee02c76f308da3dd57afca375a1b486059
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-23 13:24:44 -0500

    completed functionality on calculator

--- public/js/calculator2.js
+++ public/js/calculator2.js
@@ -7,6 +7,9 @@
   var rightInput = document.getElementById('rightInput');
   var number = document.getElementsByClassName('number');
   var operator = document.getElementsByClassName('operator');
+  var clear = document.getElementById('btnClear');
+  var equal = document.getElementById('btnEquals');
+  var answer;
 
   for(var i = 0; i < number.length; i++) {
     number[i].addEventListener('click', buttonHandler)
@@ -17,18 +20,50 @@
   }
 
   function buttonHandler() {
-    if (this.className.includes('number')) {
-      var displayValue = leftInput.getAttribute('value');
-      displayValue += this.innerText;
-      leftInput.setAttribute('value', displayValue);
-    } else if (this.className.includes('operator')) {
-      operatorInput.setAttribute('value', this.innerText);
-    } else if (operatorInput.value != '') {
+    if (this.className.includes('operator')) {
+    operatorInput.setAttribute('value', this.innerText);
+    } else if (operatorInput.value != '' && leftInput.value != '') {
       var displayValue = rightInput.getAttribute('value');
       displayValue += this.innerText;
       rightInput.setAttribute('value', displayValue);
+    } else if (this.className.includes('number')) {
+      var displayValue = leftInput.getAttribute('value');
+      displayValue += this.innerText;
+      leftInput.setAttribute('value', displayValue);
     }
   };
 
+  clear.addEventListener('click', function() {
+    leftInput.setAttribute('value', '');
+    operatorInput.setAttribute('value', '');
+    rightInput.setAttribute('value', '');
+  })
+
+
+  equal.addEventListener('click', function() {
+    var leftInputNumber = parseFloat(leftInput.value);
+    var rightInputNumber = parseFloat(rightInput.value);
+
+    switch(operatorInput.value) {
+      case '+':
+        answer = leftInputNumber + rightInputNumber;
+        break;
+      case '-':
+        answer = leftInputNumber - rightInputNumber;
+        break;
+      case '*':
+        answer = leftInputNumber * rightInputNumber;
+        break;
+      case '/':
+        if(leftInputNumber === 0 || rightInputNumber === 0) {
+          alert('Cannot divide by 0');
+        }
+        answer = leftInputNumber / rightInputNumber;
+        break;
+    }
+    console.log(answer);
+    leftInput.setAttribute('value', answer);
+    rightInput.setAttribute('value', '');
+  })
 
 }());
