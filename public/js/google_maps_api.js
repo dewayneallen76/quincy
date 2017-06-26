1e0bc7ebf4c1ea9d28b124116a0129227c20884e
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-26 10:31:26 -0500

    added google maps javascript so that it will render map

--- public/js/google_maps_api.js
+++ public/js/google_maps_api.js
@@ -1,4 +1,41 @@
 (function() {
   'use strict';
-  
+  var mapElement = document.getElementById("map");
+  var mapOptions = {
+    // Set the zoom level
+        zoom: 19,
+
+        // This sets the center of the map at our location
+        center: {
+            lat:  29.426791,
+            lng: -98.489602
+        },
+
+        mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Set our address to geocode
+  var address = "300 Alamo Plaza, San Antonio, TX, 78205";
+
+  // Init geocoder object
+  var geocoder = new google.maps.Geocoder();
+
+  // Geocode our address
+  geocoder.geocode({ "address": address }, function(results, status) {
+
+     // Check for a successful result
+     if (status == google.maps.GeocoderStatus.OK) {
+
+         // Recenter the map over the address
+         map.setCenter(results[0].geometry.location);
+     } else {
+
+         // Show an error message with the status if our request fails
+         alert("Geocoding was not successful - STATUS: " + status);
+     }
+   });
+
+
+
 }());
