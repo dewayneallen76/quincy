ddb7caa9b4c99211ade67639fd121d7a4b1b211c
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-27 14:25:36 -0500

    added to jquery exercises files for events lesson

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -5,4 +5,8 @@
   $('#main-header').css('color', 'red');
 
   $('#content').html('removing paragraph and adding this');
+
+  $('#codeup').click(function() {
+    alert('h1 with id codeup was clicked');
+  });
 }());
