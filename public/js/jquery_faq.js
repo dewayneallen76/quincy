6a5d41afc431d8d80446ea268983fe673af54dc6
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-29 13:40:29 -0500

    updated files for jquery traversing lesson

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -3,6 +3,7 @@
   console.log('connected');
 
   $(document).ready(function() {
+
     $('#make-invisible').click(function(event) {
       event.preventDefault();
       $('dd').toggleClass('invisible');
@@ -12,6 +13,21 @@
       event.preventDefault();
       $(this).toggleClass('highlighted');
     });
+
+    $('button').click(function(event) {
+      $('ul').each(function(index) {
+        $(this).children('li').last().css('background-color', '#FF0');
+      });
+    });
+
+    $('h3').click(function(event) {
+      $(this).next().children().css('font-weight', 'bold');
+    });
+
+    $('li').click(function(event) {
+      $(this).first().css('color', 'blue');
+    });
+
   });
 
 }());
