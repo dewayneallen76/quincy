609acdb9cf5a33d54f4b8ed21f7d60ea3ea272c9
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-29 09:58:03 -0500

    added h1 with class to change html when clicked

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -1,6 +1,11 @@
 (function() {
   'use strict';
   console.log('connected');
+
+  $('#codeup').click(function(event) {
+    $(this).html('Codeup Rocks!');
+  });
+
   $(document).ready(function() {
     $('#highlight-important').click(function(event) {
       event.preventDefault();
