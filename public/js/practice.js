989f2e344c22034377494f1125ef1c8b4c2d8d69
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-30 11:35:41 -0500

    no changes made

--- public/js/practice.js
+++ public/js/practice.js
@@ -5,8 +5,8 @@
   Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
   */
   function solution(string) {
-    
+  
   }
 
-  console.log(solution('abc'));
+  console.log(solution('abcd'));
 }());
