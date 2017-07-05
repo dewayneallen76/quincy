d9ddc4bc51dfbc27a338f72fd60c6dd9fc89598a
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-05 14:16:53 -0500

    added practice kata from codewars

--- public/js/practice.js
+++ public/js/practice.js
@@ -19,5 +19,19 @@
 
   console.log(mumble("abcd"));
 
+/*  Write a function that accepts a string, and returns true if it is in the form of a phone number.
+Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+Only worry about the following format:
+(123) 456-7890 (don't forget the space after the close parentheses) */
+  function validPhoneNumber(phoneNumber) {
+    if(phoneNumber.length === 14 && phoneNumber.charAt(9) === "-") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  console.log(validPhoneNumber("(423) 462-2558"));
+
 
 }());
