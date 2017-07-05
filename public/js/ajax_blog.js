47ff617414f92f5b5f52b2a3a7cec8ac454848dc
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-05 10:40:28 -0500

    added functionality to get the blog information from the data file to display

--- public/js/ajax_blog.js
+++ public/js/ajax_blog.js
@@ -1,4 +1,26 @@
 (function() {
   'use strict';
-  
+
+  var blog = 'data/blog.json';
+
+  $.get(blog).done(function(data) {
+    console.log(data);
+  });
+
+  function addBlog(blog) {
+    var content = '';
+    blog.forEach(function(blog){
+      content += '<h2>' + blog.title + '</h2>';
+      content += '<p>' + blog.content + '</p>';
+      content += '<h4>' + 'Categories: ' + blog.categories + '</h4>';
+      content += '<h4>' + 'Date: ' + blog.date + '</h4>';
+    });
+    $('#posts').append(content);
+  }
+
+  $.get(blog).done(function(blog) {
+    addBlog(blog);
+  });
+
+
 }());
