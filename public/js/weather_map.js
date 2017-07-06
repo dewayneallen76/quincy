52a5f1933ed906fc73e33976efc26098964dcf50
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-06 11:39:17 -0500

    added ajax get request to verify connection to open weather api

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -2,11 +2,11 @@
   'use strict';
   console.log('connected');
 
-  $.get("http://api.openweathermap.org/data/2.5/weather", {
+  $.get("http://api.openweathermap.org/data/2.5/forecast?", {
     APPID: "9045f328001b8cd61b813c19463bd371",
     q:     "San Antonio, TX"
   }).done(function(data){
-    console.log(data);
+      console.log(data);
   });
 
 }());
