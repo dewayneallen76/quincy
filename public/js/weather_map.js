51acf6cc8bedf071363342ffdd0cdbaf26850fb2
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-06 11:48:07 -0500

    added constant for api key and variables for lat and long

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -2,11 +2,21 @@
   'use strict';
   console.log('connected');
 
+  const apiKey = "9045f328001b8cd61b813c19463bd371";
+  var lat = "29.423017";
+  var lon = "-98.48527";
+
   $.get("http://api.openweathermap.org/data/2.5/forecast?", {
-    APPID: "9045f328001b8cd61b813c19463bd371",
-    q:     "San Antonio, TX"
+    APPID: apiKey,
+    lat: lat,
+    lon: lon,
+    unit: "imperial"
   }).done(function(data){
       console.log(data);
   });
+
+  
+
+
 
 }());
