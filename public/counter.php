a15a7e3ea44a85d8ccbcd98eff0e0c2f20517fe7
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-01 10:51:21 -0500

    refactored to use direction in query to increase and decrease the counter

--- public/counter.php
+++ public/counter.php
@@ -1,13 +1,22 @@
 <?php
 
 function pageController() {
-  $count = [];
-  $count['count'] = (isset($_GET['count'])) ? $_GET['count'] : 0;
-
-  return $count;
-
+  if(isset($_GET['direction']) && isset($_GET['count'])) {
+    $count = $_GET['count'];
+    $direction = $_GET['direction'];
+    if($direction == 'UP') {
+      $count++;
+    } else {
+      $count--;
+    }
+  } else {
+    $count = $_GET['count'] = 0;
+    $direction = $_GET['direction'] = 'START';
+  }
+  return [ 'count' => $count, 'direction' => $direction];
 }
 
+var_dump($_GET);
 extract(pageController());
 
  ?>
@@ -30,9 +39,9 @@ extract(pageController());
     <![endif]-->
   </head>
   <body>
-    <h1>Count: <?= $count; ?></h1>
-    <a href="/counter.php?count=<?= $count +1 ?>">Up</a>
-    <a href="/counter.php?count=<?= $count -1 ?>">Down</a>
+    <h1>Count:<?= $count; ?></h1>
+    <a href="/counter.php?direction=UP&count=<?= $count; ?>">Up</a>
+    <a href="/counter.php?direction=DOWN&count=<?= $count; ?>">Down</a>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
