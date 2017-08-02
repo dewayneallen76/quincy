ee17b917bc26e3a0a92fbc931c0a9d7ff528d6fe
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-02 10:40:46 -0500

    completed $_POST exercise with classroom added authorized.php file to redirect if login and password are correct

---
+++ public/login.php
@@ -0,0 +1,50 @@
+<?php
+var_dump($_POST);
+function pageController() {
+  $data = [];
+  $username = (isset($_POST['username'])) ? $_POST['username'] : '';
+  $password = (isset($_POST['password'])) ? $_POST['password'] : '';
+
+  if(!empty($_POST) || !empty($_POST)) {
+    if($username == 'guest' && $password == 'password') {
+      header("Location: /authorized.php");
+      die;
+    } else {
+      die ("Login failed");
+    }
+  }
+  return $data;
+}
+
+extract(pageController());
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <form class="" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" name="username" value="">
+      <label for="password">Password:</label>
+      <input type="password" name="password" value="">
+      <input type="submit">
+    </form>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
