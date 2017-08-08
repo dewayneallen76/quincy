af7610af87f3ff7ef2286bec391e38dc23dd6fbc
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-08 09:49:30 -0500

    Added log class

---
+++ Log.php
@@ -0,0 +1,36 @@
+<?php
+
+class Log {
+
+	public $filename;
+	public $handle;
+
+	public function __construct($prefix = 'log')
+	{
+		$this->filename = $prefix . "-" . date('Y-m-d') . ".log";
+		$this->handle = fopen($this->filename, 'a');
+	}
+
+	public function logMessage($level, $message) {
+
+		$timestamp = date("Y-m-d H:i:s");
+		$logEntry = PHP_EOL . "$timestamp - $level - $message";
+		fwrite($this->handle, $logEntry);
+
+	}
+
+	public function info($message) {
+		$this->logMessage("INFO", $message);
+	}
+
+	public function error($message) {
+		$this->logMessage("ERROR", $message);
+	}
+
+	public function __destruct()
+	{
+		fclose($this->handle);
+	}
+
+
+}
