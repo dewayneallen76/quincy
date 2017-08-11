b48e25cd879be2e8975d9672e7ac1ff1ec95d445
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-11 10:07:19 -0500

    removed echo for successful connection so that it would not show up on the page

--- db_connect.php
+++ db_connect.php
@@ -5,6 +5,6 @@ $dbc = new PDO('mysql:host='. DB_HOST . ';dbname=' . DB_NAME , DB_USER, DB_PASS)
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo  $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS).PHP_EOL;
+// echo  $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS).PHP_EOL;
 
  ?>
