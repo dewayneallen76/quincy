6ee451b3a47743e0632b1081a085c4be3fefbd33
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-10 21:01:49 -0500

    corrected concatenation for constants on the db connection

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,7 @@
 <?php
+require_once('parks_login.php');
 
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'vagrant', 'vagrant');
+$dbc = new PDO('mysql:host='. DB_HOST . ';dbname=' . DB_NAME , DB_USER, DB_PASS);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
