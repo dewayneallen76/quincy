7231b856f1777286716fd4183b101fa3f1a13524
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-11 12:02:21 -0500

    added logic for pagination

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,16 +1,56 @@
 <?php
 
 require_once './../db_connect.php';
+require_once './../Input.php';
 
-function getParks($dbc)
-{
-  $limit = 4;
-  $stmt = $dbc->query("SELECT * FROM national_parks");
-  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  return $rows;
+function getLastPage($dbc, $limit) {
+  $stmt = $dbc->query("SELECT COUNT(*) FROM national_parks");
+  $count = $stmt->fetch()[0];
+  $lastPage = ceil($count / $limit);
+  return $lastPage;
 }
 
-$parks = getParks($dbc);
+function getPaginatedParks($dbc, $page, $limit)
+{
+  $offset = ($page - 1) * $limit;
+
+  $stmt = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset");
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function handleOutOfRangeRequests($page, $lastPage)
+{
+	// protect from looking at negative pages, too high pages, and non-numeric pages
+ if($page < 1 || !is_numeric($page)) {
+		header("location: national_parks.php?page=1");
+		die;
+	} else if($page > $lastPage) {
+		header("location: national_parks.php?page=$lastPage");
+		die;
+	}
+}
+
+
+function pageController($dbc)
+{
+
+  $data = [];
+
+  $limit = 4;
+  $page = Input::get('page', 1);
+
+  $lastPage = getLastPage($dbc, $limit);
+
+  handleOutOfRangeRequests($page, $lastPage);
+
+  $data['parks'] = getPaginatedParks($dbc, $page, $limit);
+  $data['page'] = $page;
+  $data['lastPage'] = $lastPage;
+
+  return $data;
+}
+
+extract(pageController($dbc));
 
  ?>
 
@@ -51,8 +91,8 @@ $parks = getParks($dbc);
           <?php endforeach; ?>
         </tbody>
       </table>
-      <a href="national_parks.php?page=">< Prev</a>
-      <a href="national_parks.php?page=">Next ></a>
+      <a <?php if ($page <= 1): ?>style="color: grey;"<?php endif; ?> href="/national_parks.php?page=<?=$page-1?>">Previous</a>
+      <a <?php if ($page >= $lastPage): ?>style="color: grey;"<?php endif; ?> class="pull-right" href="/national_parks.php?page=<?=$page+1?>">Next</a>
     </div>
 
 
