1eed60dda36f6e3494bd12aecbd3e564ac01ecea
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-11 21:18:16 -0500

    tried adding description, will need to redo seeder, not working using the csv file by adding a description

--- parks_migration.php
+++ parks_migration.php
@@ -9,7 +9,7 @@ $createParksTable = 'CREATE TABLE IF NOT EXISTS national_parks (
   name VARCHAR(255) NOT NULL,
   location VARCHAR(255) NOT NULL,
   date_established DATE NOT NULL,
-  area_in_acres DOUBLE NOT NULL, 
+  area_in_acres DOUBLE NOT NULL,
   PRIMARY KEY (id)
 )';
 
