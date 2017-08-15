905de8587ee020887ee74062b2382c815323e24c
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-15 13:44:03 -0500

    refactored to use park model insert method

--- parks_seeder.php
+++ parks_seeder.php
@@ -1,7 +1,6 @@
 <?php
 
-require 'Park.php';
-// require_once('db_connect.php');
+require_once 'Park.php';
 
 Park::dbConnect();
 Park::$dbc->exec('TRUNCATE national_parks');
@@ -20,28 +19,16 @@ $parks = array_map('trim', $parks);
 
 foreach($parks as $park) {
   $park = explode(',', $park);
-//
-// refactor to use park model, not working currently, getting hung up on date_established
-//   $addPark = new Park();
-//   $addPark->name = $park[0];
-//   $addPark->location = $park[1];
-//   $addPark->date_established = $park[2];
-//   $addPark->area_in_acres = $park[3];
-//   $addPark->description = $park[4];
-//
-//   $addPark->insert();
 
-  $seedParksTable = "INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)";
+  $addPark = new Park();
+  $addPark->name = $park[0];
+  $addPark->location = $park[1];
+  $addPark->dateEstablished = $park[2];
+  $addPark->areaInAcres = $park[3];
+  $addPark->description = $park[4];
 
-  $stmt = Park::$dbc->prepare($seedParksTable);
-
-  $stmt->bindValue(':name', $park[0], PDO::PARAM_STR);
-  $stmt->bindValue(':location', $park[1], PDO::PARAM_STR);
-  $stmt->bindValue(':date_established', $park[2], PDO::PARAM_STR);
-  $stmt->bindValue(':area_in_acres', $park[3], PDO::PARAM_STR);
-  $stmt->bindValue(':description', $park[4], PDO::PARAM_STR);
-
-  $stmt->execute();
+  $addPark->insert();
+  
 }
 
 
